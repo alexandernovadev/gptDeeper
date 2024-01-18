f4c1051e2a8d65f7b059766564aa546fd8bb4aba
Alexander.Nova <Alexander.Nova@Contractor.Prometric.com>
2024-01-18 13:52:03 -0500

    Alex - new stream with generator functions

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -1,2 +1,4 @@
 export * from "./orthographyUseCase";
 export * from "./proconsDiscusserUseCase";
+export * from "./proconsDiscusserStreamUseCase";
+export * from "./pros-cons-stream-generator.use-case";
