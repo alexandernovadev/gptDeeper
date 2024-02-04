bec38a9582879ff41a977b1fb6a3e42b7e6254dd
alexandernovadev <alexsk88.dev@gmail.com>
2024-02-04 17:18:13 -0500

    More pages of somethig

--- src/interfaces/audioToTextResponse.ts
+++ src/interfaces/audioToTextResponse.ts
@@ -1,4 +1,3 @@
-
 export interface AudioToTextResponse {
   task: string;
   language: string;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,4 @@
 export * from "./orthographyResponse";
 export * from "./proConsResponse";
 export * from "./translateResponse";
-export * from "./assistantresponse";
+export * from "./audioToTextResponse";
